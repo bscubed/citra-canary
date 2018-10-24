4e7403985bfbca9b6ede38907652f46d5a0311ce
Brendan Szymanski <brendan.szymanski1@gmail.com>
2018-10-24 04:54:46 -0400

    Change docker run parameters

--- .travis/linux-flatpak/build.sh
+++ .travis/linux-flatpak/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 mkdir -p "$HOME/.ccache"
-docker run --env-file .travis/common/travis-ci.env -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache --cap-add SYS_ADMIN --device /dev/fuse citraemu/build-environments:linux-fresh /bin/bash -ex /citra/.travis/linux-flatpak/docker.sh
+docker run --env-file .travis/common/travis-ci.env -v $(pwd):/citra -v "$HOME/.ccache":/root/.ccache --privileged citraemu/build-environments:linux-fresh /bin/bash -ex /citra/.travis/linux-flatpak/docker.sh
