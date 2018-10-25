fbb8b37e6afd92837766fc6b4283f3c9d5ff0d97
Brendan Szymanski <brendan.szymanski1@gmail.com>
2018-10-25 03:14:08 -0400

    Compress build and upload scripts into one

--- .travis/linux-flatpak/docker.sh
+++ .travis/linux-flatpak/docker.sh
@@ -30,3 +30,17 @@ flatpak-builder --ccache --force-clean --state-dir="$STATE_DIR" --repo="$REPO_DI
 # Create a flatpak bundle
 mkdir -p $OUTPUT_DIR
 flatpak build-bundle "$REPO_DIR" "$OUTPUT_DIR/citra-canary.flatpak" org.citra.citra-canary
+
+# Push the flatpak bundle to the repo
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_b892d17adcdd_key -iv $encrypted_b892d17adcdd_iv -in "$PRIVATE_KEY.enc" -out "$PRIVATE_KEY" -d
+chmod 600 $PRIVATE_KEY
+ssh-add $PRIVATE_KEY
+chmod -R 600 "$HOME/.ssh"
+chown -R root "$HOME/.ssh"
+echo "[$SSH_HOSTNAME]:$SSH_PORT,[$(dig +short $SSH_HOSTNAME)]:$SSH_PORT $SSH_PUBLIC_KEY" > ~/.ssh/known_hosts
+mkdir "$CITRA_SRC_DIR/mnt"
+sshfs "$SSH_USER@$SSH_HOSTNAME:$SSH_LOCATION" "$CITRA_SRC_DIR/mnt" -C -p "$SSH_PORT" -o IdentityFile=$PRIVATE_KEY
+mv "$OUTPUT_DIR/citra-canary.flatpak" "$CITRA_SRC_DIR/mnt/citra-canary.flatpak"
+umount -f "$CITRA_SRC_DIR/mnt"
+rm -rf "$CITRA_SRC_DIR/mnt"
