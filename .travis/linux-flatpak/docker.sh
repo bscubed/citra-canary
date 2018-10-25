ad6eefbc03a78d0a9c0e1c4ce3908e82c667010d
Brendan Szymanski <brendan.szymanski1@gmail.com>
2018-10-25 00:51:40 -0400

    Developing with travis is haaaaard :( TODO: revert this commit later

--- .travis/linux-flatpak/docker.sh
+++ .travis/linux-flatpak/docker.sh
@@ -10,21 +10,21 @@ MAKEFLAGS="-j4"
 # Update the host packages
 apt-get -y update && apt-get -y full-upgrade
 apt-get install -y flatpak flatpak-builder ca-certificates git sshfs curl fuse
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.kde.Platform//5.11
-flatpak install -y flathub org.kde.Sdk//5.11
-flatpak install -y flathub org.freedesktop.Sdk.Extension.gcc7
+# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# flatpak install -y flathub org.kde.Platform//5.11
+# flatpak install -y flathub org.kde.Sdk//5.11
+# flatpak install -y flathub org.freedesktop.Sdk.Extension.gcc7
 
 # Download the Citra compatibility list
 curl --url https://api.citra-emu.org/gamedb/ -o "$CITRA_SRC_DIR"/.travis/linux-flatpak/compatibility_list.json
 
 # Build the citra flatpak
-flatpak-builder --ccache --force-clean --state-dir="$STATE_DIR" --repo="$REPO_DIR" "$BUILD_DIR" "$CITRA_SRC_DIR"/.travis/linux-flatpak/org.citra.citra-canary.json
+# flatpak-builder --ccache --force-clean --state-dir="$STATE_DIR" --repo="$REPO_DIR" "$BUILD_DIR" "$CITRA_SRC_DIR"/.travis/linux-flatpak/org.citra.citra-canary.json
 
 # Push a test file to the repo
 echo "[$SSH_HOSTNAME]:$SSH_PORT,[$(dig +short $SSH_HOSTNAME)]:$SSH_PORT $SSH_PUBLIC_KEY" > ~/.ssh/known_hosts
 mkdir $CITRA_SRC_DIR/mnt
-sshfs $SSH_USER@$SSH_HOSTNAME:$SSH_LOCATION $CITRA_SRC_DIR/mnt -C -p $SSH_PORT -o IdentityFile=/tmp/citra-sftp-flatpak
+sshfs $SSH_USER@$SSH_HOSTNAME:$SSH_LOCATION $CITRA_SRC_DIR/mnt -C -p $SSH_PORT -o IdentityFile=/tmp/citra-sftp-flatpak -odebug,sshfs_debug,loglevel=debug
 echo "If you see this file, that means everything works." > $CITRA_SRC_DIR/mnt/success.txt
 
 # Sign the generated repository
