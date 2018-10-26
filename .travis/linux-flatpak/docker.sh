4f012c5962161622ff8aea771e511d54b2fcb70e
Brendan Szymanski <brendan.szymanski1@gmail.com>
2018-10-26 17:34:08 -0400

    Keep the git clean and log more debugging info

--- .travis/linux-flatpak/docker.sh
+++ .travis/linux-flatpak/docker.sh
@@ -8,6 +8,19 @@ KEYS_ARCHIVE="/tmp/keys.tar"
 SSH_KEY="/tmp/ssh.key"
 GPG_KEY="/tmp/gpg.key"
 
+echo "Checking variables in docker"
+echo "CI: $CI"
+echo "TRAVIS: $TRAVIS"
+echo "CONTINUOUS_INTEGRATION: $CONTINUOUS_INTEGRATION"
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_BUILD_ID: $TRAVIS_BUILD_ID"
+echo "TRAVIS_BUILD_NUMBER: $TRAVIS_BUILD_NUMBER"
+echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
+echo "TRAVIS_JOB_ID: $TRAVIS_JOB_ID"
+echo "TRAVIS_JOB_NUMBER: $TRAVIS_JOB_NUMBER"
+echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+echo "TRAVIS_TAG: $TRAVIS_TAG"
+
 # Update the host packages
 apt-get install -y flatpak flatpak-builder ca-certificates git sshfs curl fuse dnsutils gnupg2
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -29,7 +42,7 @@ echo "[$SSH_HOSTNAME]:$SSH_PORT,[$(dig +short $SSH_HOSTNAME)]:$SSH_PORT $SSH_PUB
 gpg2 --import "$GPG_KEY"
 
 # Download the Citra compatibility list
-curl --url https://api.citra-emu.org/gamedb/ -o "$CITRA_SRC_DIR/.travis/linux-flatpak/compatibility_list.json"
+curl --url https://api.citra-emu.org/gamedb/ -o "/tmp/compatibility_list.json"
 
 # Mount our flatpak repository
 mkdir -p "$REPO_DIR"
